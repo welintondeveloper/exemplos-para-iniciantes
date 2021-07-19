46130b1bc592c908913825abfea5d449adc0e6c2
welintondeveloper <86738578+welintondeveloper@users.noreply.github.com>
2021-07-19 11:31:39 -0300

    aluno aprovado ou reprovado

---
+++ exemplo1.js
@@ -0,0 +1,23 @@
+// exemplos de estruturas de controle if else switch
+// Criar um programa que diz se o aluno foi aprovado
+// com base na nota if a nota for maior que 60 aprovado
+// else nota menor que 60 reprobado
+// condição três se o aluno tira menos que 50 
+// mensagem estude mais se tira acima de 80 parabéns
+nota = 60;
+
+if(nota <= 60){
+  console.log("reprovado");
+
+}else if(nota <= 40){
+  console.log("estude mais");
+}else if(>=80){
+  console.log("parabéns");
+}else if(nota>=60){
+  console.log("aprovado");
+}
+
+console.log("boas férias");
+
+
+
